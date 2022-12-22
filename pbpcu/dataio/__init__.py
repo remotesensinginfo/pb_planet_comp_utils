9cd50c4342b2567e4317353123e343a6191aef03
Pete Bunting <petebunting@mac.com>
2022-12-22 13:24:21 +0000

    Add function to check urls are correct.

--- pbpcu/dataio/__init__.py
+++ pbpcu/dataio/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import rasterio
+import urllib.request
+import urllib.error
+import time
 
 
 def get_img_metadata(img_file):
@@ -19,3 +22,26 @@ def get_img_band_array(img_file, band=1):
     img_arr = img_data_obj.read(band)
     img_data_obj = None
     return img_arr
+
+
+def test_asset_urls(signed_items):
+    chkd_items = list()
+    for scn_item in signed_items:
+        assets_present = True
+        for asset_name in scn_item.assets:
+            try:
+                if (
+                    urllib.request.urlopen(scn_item.assets[asset_name].href).getcode()
+                    != 200
+                ):
+                    assets_present = False
+                    break
+            except urllib.error.HTTPError:
+                assets_present = False
+                break
+            time.sleep(0.1)
+        if assets_present:
+            chkd_items.append(scn_item)
+    print(f"Before: {len(signed_items)}")
+    print(f"After: {len(chkd_items)}")
+    return chkd_items
